0310021c785a8d8cf8b8a491bb6e0257705907c9
co191194 <co191194@outlook.com>
2025-04-09 22:56:16 +0900

    dockerでマウントしないように変更＆ボリューム作成時にSQLを実行するように変更

---
+++ dev/sql/init/insert_test_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+    account (user_id, user_pw, user_name, created_by, created_at)
+VALUES
+    ('test1', '$2b$12$jgN3M4fuP2T8xeous3uTbumFkooq4mFBYsWVeEG63S6iZsnBIDjCG', 'テスト１', 'SYSTEM', CURRENT_TIMESTAMP);
